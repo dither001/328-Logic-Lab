952895e8f4a05d07ca5b9794762408d8ba9168d4
dither001 <32025687+dither001@users.noreply.github.com>
2019-03-05 11:25:22 -0700

    Adder4 compiles
    Compiles but incomplete

--- src/edu/westminstercollege/cmpt328/submission/Adder4.java
+++ src/edu/westminstercollege/cmpt328/submission/Adder4.java
@@ -3,16 +3,20 @@ package edu.westminstercollege.cmpt328.submission;
 import edu.westminstercollege.cmpt328.logic.*;
 
 public class Adder4 {
+	// Inputs to the circuit: InputLines
 	public final InputMultiLine A = new InputMultiLine(4);
 	public final InputMultiLine B = new InputMultiLine(4);
-//	public final MultiLine SUM = new MultiLine(4);
+	public final InputLine Cin = new InputLine();
 
 	// Outputs of the circuit: Lines (initialize in constructor)
 	public final MultiLine SUM;
-	public final InputLine Cin;
 	public final Line Cout;
 
 	public Adder4() {
+		FullAdder[] adders = new FullAdder[4];
 		
+		
+		SUM = null;
+		Cout = null;
 	}
 }
